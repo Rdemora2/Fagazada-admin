54b5bc585410666c8e32b774819377eaf09130e5
Rdemora2 <robertomoraeszar@gmail.com>
2024-06-16 07:09:33 -0300

    STYLE/ estiliza tela inicial de boas vindas

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 import {WelcomeScreenNavigationProp} from '../types/types';
 
 type Props = {
@@ -7,14 +7,25 @@ type Props = {
 };
 
 const WelcomeScreen: React.FC<Props> = ({navigation}) => {
+  const handleLoginPress = () => {
+    navigation.navigate('Login');
+  };
+
+  const handleRegisterPress = () => {
+    navigation.navigate('Register');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bem-vindo ao Fagazada Gestores!</Text>
-      <Button title="Login" onPress={() => navigation.navigate('Login')} />
-      <Button
-        title="Registrar"
-        onPress={() => navigation.navigate('Register')}
-      />
+      <TouchableOpacity style={styles.button} onPress={handleLoginPress}>
+        <Text style={styles.buttonText}>Fazer login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, styles.registerButton]}
+        onPress={handleRegisterPress}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -24,10 +35,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f6f6f6',
+    padding: 16,
   },
   title: {
     fontSize: 24,
-    marginBottom: 20,
+    fontWeight: 'bold',
+    marginBottom: 40,
+    textAlign: 'center',
+    color: '#00786A',
+  },
+  button: {
+    width: '100%',
+    marginTop: 12,
+    backgroundColor: '#00786A',
+    paddingVertical: 12,
+    borderRadius: 16,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  registerButton: {
+    backgroundColor: '#E66901',
   },
 });
 
