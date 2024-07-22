1e724c23c99dec2b3668f3eecc5030962934ce31
Rdemora2 <robertomoraeszar@gmail.com>
2024-07-22 01:50:05 -0300

    STYLE/ estilização final do pop up

--- src/screens/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen.tsx
@@ -141,14 +141,20 @@ const EditProfileScreen: React.FC<Props> = ({route, navigation}) => {
         />
       </View>
 
-      <Modal visible={isModalVisible}>
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalText}>Perfil Atualizado!</Text>
-          <TouchableOpacity
-            style={styles.modalButton}
-            onPress={handleModalClose}>
-            <Text style={styles.modalButtonText}>OK</Text>
-          </TouchableOpacity>
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={handleModalClose}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>Perfil Atualizado!</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={handleModalClose}>
+              <Text style={styles.modalButtonText}>OK</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </Modal>
     </View>
@@ -201,15 +207,22 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
   modalContainer: {
+    width: '50%',
     backgroundColor: 'white',
-    padding: 20,
+    padding: 25,
     borderRadius: 10,
     alignItems: 'center',
   },
   modalText: {
     fontSize: 18,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   modalButton: {
     backgroundColor: '#00786A',
